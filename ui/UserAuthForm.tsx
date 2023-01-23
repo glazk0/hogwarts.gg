aa15757c5566e07b677211fea1827067996bfbc5
lmachens <leon@machens.koeln>
2023-01-23 20:50:56 +0100

    Replace plausible library [next-plausible](https://github.com/4lejandrito/next-plausible) seems to more suitable with Next.js 13. It allows us to automatically track outbound links (and more, see docs). After this is merged, I like to check if this already solves the issue that access_tokens are shared on Plausible after sign-in.

--- ui/UserAuthForm.tsx
+++ ui/UserAuthForm.tsx
@@ -19,7 +19,6 @@ const getURL = () => {
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-  console.log(url);
   return url;
 };
 
