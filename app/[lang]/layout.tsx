bcebce99c3dbe6528e1f642898a961ff13a196fe
lmachens <leon@machens.koeln>
2023-02-02 20:20:51 +0100

    Add hero to blog page In upcoming PRs, I like to create a component from this and add a parallax effect.
    The colors are slighty changed too (not final)

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -11,6 +11,7 @@ import type { ReactNode } from 'react';
 const fontSerif = localFont({
   variable: '--font-serif',
   src: './fonts/animales-fantastic.woff2',
+  display: 'block',
 });
 
 const fontSans = WorkSans({
@@ -32,7 +33,7 @@ const RootLayout = async ({
       lang={lang}
       dir="ltr"
       className={cn(
-        '[color-scheme:dark] select-none bg-gray-1100',
+        '[color-scheme:dark] select-none bg-gradient-to-b from-gray-1000 via-gray-1000 to-gray-1100 text-white scroll-smooth',
         fontSerif.variable,
         fontSans.variable,
       )}
