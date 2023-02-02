e06fea18f41192b657c4f655217a6429bb871fe8
Théo <43091603+glazk0@users.noreply.github.com>
2023-02-02 20:43:54 +0100

    Formatted

--- lib/posts.ts
+++ lib/posts.ts
@@ -11,6 +11,7 @@ export const getPostBySlug = async (slug: string): Promise<Post | null> => {
     )
     .eq('slug', slug)
     .maybeSingle();
+
   if (error) {
     throw error;
   }
@@ -21,14 +22,17 @@ export const getPostBySlug = async (slug: string): Promise<Post | null> => {
 
   // It will never be an array because the `users.id` is unique
   ok(!Array.isArray(post.user));
+
   // The author should exists
   ok(post.user !== null);
   ok(Array.isArray(post.posts));
+
   if (post.parent) {
     post.posts.push(
       post.parent as { id: number; language: string; slug: string },
     );
   }
+
   return {
     ...post,
     user: post.user,
@@ -38,9 +42,11 @@ export const getPostBySlug = async (slug: string): Promise<Post | null> => {
 
 export const getPostById = async (postId: string): Promise<Post | null> => {
   const id = +postId;
+
   if (Number.isNaN(id)) {
     return null;
   }
+
   const { data: post, error } = await supabase
     .from('posts')
     .select(
@@ -48,6 +54,7 @@ export const getPostById = async (postId: string): Promise<Post | null> => {
     )
     .eq('id', id)
     .maybeSingle();
+
   if (error) {
     throw error;
   }
@@ -58,9 +65,11 @@ export const getPostById = async (postId: string): Promise<Post | null> => {
 
   // It will never be an array because the `users.id` is unique
   ok(!Array.isArray(post.user));
+
   // The author should exists
   ok(post.user !== null);
   ok(Array.isArray(post.posts));
+
   if (post.parent) {
     post.posts.push(
       post.parent as { id: number; language: string; slug: string },
@@ -81,9 +90,11 @@ export const getPosts = async ({
     .from('posts')
     .select('*, user:users(username), posts(id, language, slug)')
     .order('published_at', { ascending: false });
+
   if (language) {
     request.eq('language', language);
   }
+
   const { data: posts, error } = await request;
 
   if (error) {
