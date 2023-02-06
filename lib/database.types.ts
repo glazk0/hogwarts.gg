cea3dbfd5d8ef1a6a92d52f7729710be0847d14b
lmachens <leon@machens.koeln>
2023-02-06 09:46:55 +0100

    Add authorized users permissions

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -4,340 +4,330 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   graphql_public: {
     Tables: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       graphql: {
         Args: {
-          operationName: string
-          query: string
-          variables: Json
-          extensions: Json
-        }
-        Returns: Json
-      }
-    }
+          operationName: string;
+          query: string;
+          variables: Json;
+          extensions: Json;
+        };
+        Returns: Json;
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
   public: {
     Tables: {
       comments: {
         Row: {
-          body: string
-          created_at: string | null
-          id: number
-          node_id: number | null
-          post_id: number | null
-          user_id: string
-        }
+          body: string;
+          created_at: string | null;
+          id: number;
+          node_id: number | null;
+          post_id: number | null;
+          user_id: string;
+        };
         Insert: {
-          body: string
-          created_at?: string | null
-          id?: number
-          node_id?: number | null
-          post_id?: number | null
-          user_id?: string
-        }
+          body: string;
+          created_at?: string | null;
+          id?: number;
+          node_id?: number | null;
+          post_id?: number | null;
+          user_id?: string;
+        };
         Update: {
-          body?: string
-          created_at?: string | null
-          id?: number
-          node_id?: number | null
-          post_id?: number | null
-          user_id?: string
-        }
-      }
+          body?: string;
+          created_at?: string | null;
+          id?: number;
+          node_id?: number | null;
+          post_id?: number | null;
+          user_id?: string;
+        };
+      };
       nodes: {
         Row: {
-          coordinates: number[]
-          created_at: string
-          description: string | null
-          id: number
-          title: string
-          type: string
-        }
+          coordinates: number[];
+          created_at: string;
+          description: string | null;
+          id: number;
+          title: string;
+          type: string;
+        };
         Insert: {
-          coordinates: number[]
-          created_at?: string
-          description?: string | null
-          id?: number
-          title: string
-          type: string
-        }
+          coordinates: number[];
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          title: string;
+          type: string;
+        };
         Update: {
-          coordinates?: number[]
-          created_at?: string
-          description?: string | null
-          id?: number
-          title?: string
-          type?: string
-        }
-      }
+          coordinates?: number[];
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          title?: string;
+          type?: string;
+        };
+      };
       posts: {
         Row: {
-          body: string | null
-          group_id: number | null
-          id: number
-          image: string | null
-          language: string
-          published: boolean
-          published_at: string | null
-          short: string | null
-          slug: string | null
-          title: string | null
-          user_id: string
-        }
+          body: string | null;
+          group_id: number | null;
+          id: number;
+          image: string | null;
+          language: string;
+          published: boolean;
+          published_at: string | null;
+          short: string | null;
+          slug: string | null;
+          title: string | null;
+          user_id: string;
+        };
         Insert: {
-          body?: string | null
-          group_id?: number | null
-          id?: number
-          image?: string | null
-          language?: string
-          published?: boolean
-          published_at?: string | null
-          short?: string | null
-          slug?: string | null
-          title?: string | null
-          user_id?: string
-        }
+          body?: string | null;
+          group_id?: number | null;
+          id?: number;
+          image?: string | null;
+          language?: string;
+          published?: boolean;
+          published_at?: string | null;
+          short?: string | null;
+          slug?: string | null;
+          title?: string | null;
+          user_id?: string;
+        };
         Update: {
-          body?: string | null
-          group_id?: number | null
-          id?: number
-          image?: string | null
-          language?: string
-          published?: boolean
-          published_at?: string | null
-          short?: string | null
-          slug?: string | null
-          title?: string | null
-          user_id?: string
-        }
-      }
+          body?: string | null;
+          group_id?: number | null;
+          id?: number;
+          image?: string | null;
+          language?: string;
+          published?: boolean;
+          published_at?: string | null;
+          short?: string | null;
+          slug?: string | null;
+          title?: string | null;
+          user_id?: string;
+        };
+      };
       role_permissions: {
         Row: {
-          id: number
-          permission: Database["public"]["Enums"]["app_permission"]
-          role: Database["public"]["Enums"]["app_role"]
-        }
+          id: number;
+          permission: Database['public']['Enums']['app_permission'];
+          role: Database['public']['Enums']['app_role'];
+        };
         Insert: {
-          id?: number
-          permission: Database["public"]["Enums"]["app_permission"]
-          role: Database["public"]["Enums"]["app_role"]
-        }
+          id?: number;
+          permission: Database['public']['Enums']['app_permission'];
+          role: Database['public']['Enums']['app_role'];
+        };
         Update: {
-          id?: number
-          permission?: Database["public"]["Enums"]["app_permission"]
-          role?: Database["public"]["Enums"]["app_role"]
-        }
-      }
+          id?: number;
+          permission?: Database['public']['Enums']['app_permission'];
+          role?: Database['public']['Enums']['app_role'];
+        };
+      };
       user_roles: {
         Row: {
-          id: number
-          role: Database["public"]["Enums"]["app_role"]
-          user_id: string
-        }
+          id: number;
+          role: Database['public']['Enums']['app_role'];
+          user_id: string;
+        };
         Insert: {
-          id?: number
-          role: Database["public"]["Enums"]["app_role"]
-          user_id: string
-        }
+          id?: number;
+          role: Database['public']['Enums']['app_role'];
+          user_id: string;
+        };
         Update: {
-          id?: number
-          role?: Database["public"]["Enums"]["app_role"]
-          user_id?: string
-        }
-      }
+          id?: number;
+          role?: Database['public']['Enums']['app_role'];
+          user_id?: string;
+        };
+      };
       users: {
         Row: {
-          id: string
-          username: string
-        }
+          id: string;
+          username: string;
+        };
         Insert: {
-          id: string
-          username: string
-        }
+          id: string;
+          username: string;
+        };
         Update: {
-          id?: string
-          username?: string
-        }
-      }
-    }
+          id?: string;
+          username?: string;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       authorize: {
         Args: {
-          requested_permission: Database["public"]["Enums"]["app_permission"]
-          user_id: string
-        }
-        Returns: boolean
-      }
-    }
+          requested_permission: Database['public']['Enums']['app_permission'];
+          user_id: string;
+        };
+        Returns: boolean;
+      };
+    };
     Enums: {
       app_permission:
-        | "users.delete"
-        | "users.edit"
-        | "posts.read"
-        | "posts.create"
-        | "posts.delete"
-        | "posts.edit"
-        | "posts.upload"
-        | "nodes.edit"
-        | "nodes.delete"
-        | "comments.edit"
-        | "comments.delete"
-      app_role: "admin" | "moderator"
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+        | 'users.delete'
+        | 'users.edit'
+        | 'posts.read'
+        | 'posts.create'
+        | 'posts.delete'
+        | 'posts.edit'
+        | 'posts.upload'
+        | 'nodes.edit'
+        | 'nodes.delete'
+        | 'comments.edit'
+        | 'comments.delete';
+      app_role: 'admin' | 'moderator';
+    };
+  };
   storage: {
     Tables: {
       buckets: {
         Row: {
-          created_at: string | null
-          id: string
-          name: string
-          owner: string | null
-          public: boolean | null
-          updated_at: string | null
-        }
+          created_at: string | null;
+          id: string;
+          name: string;
+          owner: string | null;
+          public: boolean | null;
+          updated_at: string | null;
+        };
         Insert: {
-          created_at?: string | null
-          id: string
-          name: string
-          owner?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
+          created_at?: string | null;
+          id: string;
+          name: string;
+          owner?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
         Update: {
-          created_at?: string | null
-          id?: string
-          name?: string
-          owner?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
-      }
+          created_at?: string | null;
+          id?: string;
+          name?: string;
+          owner?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
+      };
       migrations: {
         Row: {
-          executed_at: string | null
-          hash: string
-          id: number
-          name: string
-        }
+          executed_at: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
         Insert: {
-          executed_at?: string | null
-          hash: string
-          id: number
-          name: string
-        }
+          executed_at?: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
         Update: {
-          executed_at?: string | null
-          hash?: string
-          id?: number
-          name?: string
-        }
-      }
+          executed_at?: string | null;
+          hash?: string;
+          id?: number;
+          name?: string;
+        };
+      };
       objects: {
         Row: {
-          bucket_id: string | null
-          created_at: string | null
-          id: string
-          last_accessed_at: string | null
-          metadata: Json | null
-          name: string | null
-          owner: string | null
-          path_tokens: string[] | null
-          updated_at: string | null
-        }
+          bucket_id: string | null;
+          created_at: string | null;
+          id: string;
+          last_accessed_at: string | null;
+          metadata: Json | null;
+          name: string | null;
+          owner: string | null;
+          path_tokens: string[] | null;
+          updated_at: string | null;
+        };
         Insert: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
-        }
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+        };
         Update: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
-        }
-      }
-    }
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       extension: {
-        Args: { name: string }
-        Returns: string
-      }
+        Args: { name: string };
+        Returns: string;
+      };
       filename: {
-        Args: { name: string }
-        Returns: string
-      }
+        Args: { name: string };
+        Returns: string;
+      };
       foldername: {
-        Args: { name: string }
-        Returns: string[]
-      }
+        Args: { name: string };
+        Returns: string[];
+      };
       get_size_by_bucket: {
-        Args: Record<PropertyKey, never>
-        Returns: { size: number; bucket_id: string }[]
-      }
+        Args: Record<PropertyKey, never>;
+        Returns: { size: number; bucket_id: string }[];
+      };
       search: {
         Args: {
-          prefix: string
-          bucketname: string
-          limits: number
-          levels: number
-          offsets: number
-          search: string
-          sortcolumn: string
-          sortorder: string
-        }
+          prefix: string;
+          bucketname: string;
+          limits: number;
+          levels: number;
+          offsets: number;
+          search: string;
+          sortcolumn: string;
+          sortorder: string;
+        };
         Returns: {
-          name: string
-          id: string
-          updated_at: string
-          created_at: string
-          last_accessed_at: string
-          metadata: Json
-        }[]
-      }
-    }
+          name: string;
+          id: string;
+          updated_at: string;
+          created_at: string;
+          last_accessed_at: string;
+          metadata: Json;
+        }[];
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
-
