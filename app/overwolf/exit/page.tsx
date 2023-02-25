02715a576efe2fdeb9b6c2fbe46aa597c36d49d4
lmachens <leon@machens.koeln>
2023-02-25 10:05:17 +0100

    Move exit component

--- app/overwolf/exit/page.tsx
+++ app/overwolf/exit/page.tsx
@@ -1,33 +1,5 @@
-'use client';
-
-import Button from '#/ui/Button';
-import { useEffect, useState } from 'react';
+import Exit from '#/ui/overwolf/Exit';
 
 export default function Page() {
-  const [seconds, setSeconds] = useState(5);
-
-  useEffect(() => {
-    if (seconds < 1) {
-      window.close();
-    }
-  }, [seconds]);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setSeconds((s) => s - 1);
-    }, 1000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return (
-    <div className="grid gap-2 place-content-center h-screen text-center">
-      <h1 className="text-xl">Successfully logged in 🤘</h1>
-      <p>This window will be self-destroyed in {seconds} seconds</p>
-      <Button onClick={() => window.close()} className="w-60 mx-auto">
-        Close now 🚀
-      </Button>
-    </div>
-  );
+  return <Exit />;
 }
---
+++ ui/overwolf/Exit.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Button from '#/ui/Button';
+import { useEffect, useState } from 'react';
+
+export default function Exit() {
+  const [seconds, setSeconds] = useState(5);
+
+  useEffect(() => {
+    if (seconds < 1) {
+      window.close();
+    }
+  }, [seconds]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((s) => s - 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className="grid gap-2 place-content-center h-screen text-center">
+      <h1 className="text-xl">Successfully logged in 🤘</h1>
+      <p>This window will be self-destroyed in {seconds} seconds</p>
+      <Button onClick={() => window.close()} className="w-60 mx-auto">
+        Close now 🚀
+      </Button>
+    </div>
+  );
+}
