4f152f984ef3bd010bf120ffaf0285e1a7b90773
lmachens <leon@machens.koeln>
2023-02-27 11:49:40 +0100

    Display discovered nodes

--- ui/map/Nodes.tsx
+++ ui/map/Nodes.tsx
@@ -1,44 +1,55 @@
 'use client';
 
 import { useNodes } from '#/lib/hooks/use-nodes';
+import { useSavegamePlayer } from '#/lib/hooks/use-savegame-player';
 import { getNodeType } from '#/lib/node-types';
+import type { Node } from '#/lib/nodes';
 import { createNodeTooltip } from '#/lib/tooltips';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import Marker from './Marker';
 import Text from './Text';
 
 export default function Nodes({ lang }: { lang: string }) {
   const searchParams = useSearchParams()!;
   const level = +searchParams.get('level')!;
-  const router = useRouter();
   const { data: nodes = [] } = useNodes({ level, language: lang });
+  const { data: player } = useSavegamePlayer();
 
+  function isDiscovered(node: Node) {
+    if (!player) {
+      return null;
+    }
+    if (
+      node.type === 'fastTravelFireplaces' ||
+      node.type === 'fastTravelSanctuaryHogwarts'
+    ) {
+      return player.locations.hogwarts.fastTravels.values.includes(
+        node.titleId!,
+      );
+    }
+    return null;
+  }
   return (
     <>
-      {nodes!.map((node) =>
-        node.type === 'text' ? (
-          <Text key={node.id} latLng={[node.y, node.x]}>
-            {node.title!}
-          </Text>
-        ) : (
+      {nodes!.map((node) => {
+        if (node.type === 'text') {
+          return (
+            <Text key={node.id} latLng={[node.y, node.x]}>
+              {node.title!}
+            </Text>
+          );
+        }
+        const discovered = isDiscovered(node);
+        const nodeType = getNodeType(node.type);
+        return (
           <Marker
             key={node.id}
-            src={getNodeType(node.type).icon}
+            src={(discovered && nodeType.discoveredIcon) || nodeType.icon}
             latLng={[node.y, node.x] as [number, number]}
-            tooltip={createNodeTooltip(node)}
-            onContextMenu={
-              ['stairs_up', 'stairs_down'].includes(node.type) &&
-              (() => {
-                if (node.type === 'stairs_up') {
-                  router.push(`/${lang}/map/hogwarts?level=${level + 1}`);
-                } else if (node.type === 'stairs_down') {
-                  router.push(`/${lang}/map/hogwarts?level=${level - 1}`);
-                }
-              })
-            }
+            tooltip={createNodeTooltip(node, isDiscovered(node))}
           />
-        ),
-      )}
+        );
+      })}
     </>
   );
 }
