42d2d178ee3f3d7ca16439e5a7e09ef720ed8d3a
lmachens <leon@machens.koeln>
2023-02-27 17:08:43 +0100

    Remove unused function

--- lib/hooks/use-comments.ts
+++ lib/hooks/use-comments.ts
@@ -1,10 +1,6 @@
 import useSWR from 'swr';
 import { getComments } from '../comments';
 
-export function useNodeComments(nodeId: number) {
-  return useSWR(`comments/nodes/${nodeId}`, () => getComments({ nodeId }));
-}
-
 export function usePostComments(postId: number) {
   return useSWR(`comments/posts/${postId}`, () => getComments({ postId }));
 }
