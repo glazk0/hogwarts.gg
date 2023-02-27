a60cecb49a528f8fb0fb42d46dbbd4b7e9e350dc
lmachens <leon@machens.koeln>
2023-02-27 08:33:56 +0100

    Verify that Overwolf sign-in works

--- ui/overwolf/Exit.tsx
+++ ui/overwolf/Exit.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useMe } from '#/lib/hooks/use-me';
 import Button from '#/ui/Button';
 import { useEffect, useState } from 'react';
 
 export default function Exit() {
   const [seconds, setSeconds] = useState(5);
+  const { data: me, isLoading } = useMe();
 
   useEffect(() => {
     if (seconds < 1) {
@@ -13,21 +15,42 @@ export default function Exit() {
   }, [seconds]);
 
   useEffect(() => {
+    if (!me) {
+      return;
+    }
     const intervalId = setInterval(() => {
       setSeconds((s) => s - 1);
     }, 1000);
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [me]);
 
+  let content;
+  if (!isLoading) {
+    content = <></>;
+  }
+
+  if (!me) {
+    content = (
+      <>
+        <h1 className="text-xl">Could not sign-in 😡</h1>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <h1 className="text-xl">Successfully logged in 🤘</h1>
+        <p>This window will be self-destroyed in {seconds} seconds</p>
+        <Button onClick={() => window.close()} className="w-60 mx-auto">
+          Close now 🚀
+        </Button>
+      </>
+    );
+  }
   return (
     <div className="grid gap-2 place-content-center h-screen text-center">
-      <h1 className="text-xl">Successfully logged in 🤘</h1>
-      <p>This window will be self-destroyed in {seconds} seconds</p>
-      <Button onClick={() => window.close()} className="w-60 mx-auto">
-        Close now 🚀
-      </Button>
+      {content}
     </div>
   );
 }
