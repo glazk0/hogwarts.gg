e6dbcd7a2f48a1f31613bb6986adb9d3d583f368
lmachens <leon@machens.koeln>
2023-03-01 15:08:51 +0100

    Prevent dragging elements

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -38,7 +38,7 @@ const RootLayout = async ({
       lang={lang}
       dir="ltr"
       className={cn(
-        '[color-scheme:dark] select-none bg-gradient-to-b from-gray-1000 via-gray-1000 to-gray-1100 text-white scroll-smooth',
+        '[color-scheme:dark] select-none drag-none bg-gradient-to-b from-gray-1000 via-gray-1000 to-gray-1100 text-white scroll-smooth',
         fontSerif.variable,
         fontSans.variable,
       )}
